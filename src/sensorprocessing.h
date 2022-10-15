8f07dc0d2ee9cc9efa8a40fb4d60b0bd05ae5475
Shrim <shrimoyeesen01@gmail.com>
2022-10-16 02:41:21 +1100

    visual info

--- src/sensorprocessing.h
+++ src/sensorprocessing.h
@@ -1,6 +1,25 @@
-//#ifndef SENSORPROCESSING_H
-//#define SENSORPROCESSING_H
+#ifndef SENSORPROCESSING_H
+#define SENSORPROCESSING_H
 
+#include <ros/ros.h>
+#include "geometry_msgs/Pose"
+
+
+class ProcessObjects
+{
+    public:
+    {
+        ProcessObjects(ros::NodeHandle nh);
+        ros::NodeHandle nh_;
+    }
+
+    private
+    {
+        ros::Subscriber sub_;
+        ros::Publisher pub_;
+        geometry_msgs::Pose object(const std::string obj);
+    }
+}
 //class SensorProcessing
 //{
 //  //Constructor
@@ -10,4 +29,5 @@
 //  ~SensorProcessing();
 //};
 
-//#endif // SENSORPROCESSING_H
+#endif // SENSORPROCESSING_H
+
